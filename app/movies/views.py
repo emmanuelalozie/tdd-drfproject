ba3a9efff3c6437bfaf497dcf22e7c799f1f51e0
emmanuelalozie <emanalozie@gmail.com>
2024-02-13 00:15:16 -0800

    TDD-04 Viewsets, QuerySets, DRF Routing, tests.

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# app/movies/views.py
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.http import Http404
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise Http404
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
